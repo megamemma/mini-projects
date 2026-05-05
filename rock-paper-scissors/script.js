6caa0923cdad47d97d95a8dfa55170596be257e6
megamemma <sireneviyy@gmail.com>
2026-05-05 18:55:45 +0500

    Implement 5-round game logic in playGame func

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -11,11 +11,67 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    let userChoice = prompt("Pick rock, paper or scissors");
-    return userChoice.toLowerCase();
+    return prompt("Pick rock, paper or scissors").toLowerCase();
 }
 
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
+function playGame() {
+//keeping score for the computer and the user:
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(`Computer chose: ${computerChoice}, User chose: ${humanChoice}`);
+    function playRound (humanChoice, computerChoice) {
+        //in case of a tie:
+        if (humanChoice === computerChoice) {
+            console.log(`It's a tie! Both chose ${humanChoice}`);
+            return;
+        }
+        //in case of human winning:
+        if (humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            humanScore ++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`); 
+        }
+        //in case of computer winning, so, not a tie, not user's win:
+        else {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        }
+    }
+
+// Playing 5 rounds "manually", before the loops in-depth knowledge:
+const humanSelection1 = getHumanChoice();
+const computerSelection1 = getComputerChoice();
+playRound(humanSelection1, computerSelection1);
+
+const humanSelection2 = getHumanChoice();
+const computerSelection2 = getComputerChoice();
+playRound(humanSelection2, computerSelection2);
+
+const humanSelection3 = getHumanChoice();
+const computerSelection3 = getComputerChoice();
+playRound(humanSelection3, computerSelection3);
+
+const humanSelection4 = getHumanChoice();
+const computerSelection4 = getComputerChoice();
+playRound(humanSelection4, computerSelection4);
+
+const humanSelection5 = getHumanChoice();
+const computerSelection5 = getComputerChoice();
+playRound(humanSelection5, computerSelection5);
+
+console.log("--- FINAL SCORE ---");
+console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
+
+if (humanScore > computerScore) {
+        console.log("Congrats! You are the overall winner!");
+    } else if (humanScore < computerScore) {
+        console.log("Game over! Computer wins the match.");
+    } else {
+        console.log("Overall result: It's a draw!");
+    }
+
+}
+
+playGame();
