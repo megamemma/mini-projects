87496d0dfc67d06e0910f1c016e3006aae6f2ead
megamemma <sireneviyy@gmail.com>
2026-05-05 15:21:01 +0500

    Add functions to get computer and human choices

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,21 @@
-console.log("Hi bitches");
+function getComputerChoice () {  
+    let randomNum = Math.random();
+
+    if (randomNum < 1/3) {
+        return "rock";
+    } else if (randomNum < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    } 
+}
+
+function getHumanChoice () {
+    let userChoice = prompt("Pick rock, paper or scissors");
+    return userChoice.toLowerCase();
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+console.log(`Computer chose: ${computerChoice}, User chose: ${humanChoice}`);
