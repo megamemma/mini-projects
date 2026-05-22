2e35769a0f4880ffb9bdf2243c61e58cba5d13ef
megamemma <sireneviyy@gmail.com>
2026-05-22 11:52:42 +0500

    Add basic layout, style, buttons for calc

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) {
+    if (b === 0) return `Won't happen.`;
+    return a / b;
+}
+
+let num1 = "";
+let oper = "";
+let num2 = "";
+
+function operate(oper, num1, num2) {
+    if (oper === "" || num1 === "" || num2 === "") {
+        return `Won't happen.`;
+    }
+    
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (oper === "+") return add(num1, num2);
+    if (oper === "-") return subtract(num1, num2);
+    if (oper === "*") return multiply(num1, num2);
+    if (oper === "/") return divide(num1, num2);
+
+    return "Unknown operator.";
+}
