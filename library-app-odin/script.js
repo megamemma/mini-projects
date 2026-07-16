7ed7946fe53b3dc58fd92f9da64fc8b4b53649bf
megamemma <sireneviyy@gmail.com>
2026-07-16 22:29:59 +0500

    Add form called on the New-Book button click

--- library-app-odin/script.js
+++ library-app-odin/script.js
@@ -1,3 +1,6 @@
+// Create an array:
+const myLibrary = [];
+
 class Book {
     constructor(title, author, pages, read) {
         this.id = crypto.randomUUID()
@@ -6,21 +9,58 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
-
-    info() {
-        const readStatus = this.read ? "Already read" : "Not read yet";
-        return `${this.title} by ${this.author} has ${this.pages}. ${readStatus}`;
-    }
 }
 
-// Create an array:
-const myLibrary = [];
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);  //create an obj instance
-    myLibrary.push(newBook); //push it into the array
+    const newBook = new Book(title, author, pages, read);  
+    myLibrary.push(newBook); 
 } 
 
-// example of the call:
+function displayBooks() {
+    const grid = document.querySelector(".grid");
+    grid.innerHTML = "";
+
+    myLibrary.forEach(  (book) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Status: ${book.read ? "Read already" : "Not read"}</p>
+        `;
+        grid.appendChild(card);
+    });
+}
+
 addBookToLibrary("The Hobbit", "J. Tolkien", 295, false);
-console.log(myLibrary);
+addBookToLibrary("1984", "G. Orwell", 328, true);
+addBookToLibrary("1984", "G. Orwell", 328, true);
+addBookToLibrary("1984", "G. Orwell", 328, true);
+addBookToLibrary("1984", "G. Orwell", 328, true);
+addBookToLibrary("1984", "G. Orwell", 328, true);
+
+displayBooks();
+
+const dialog = document.getElementById('book-dialog');
+const form = document.getElementById('book-form');
+
+document.getElementById('open-form').addEventListener('click', () => dialog.showModal());
+document.getElementById('close-form').addEventListener('click', () => dialog.close());
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = formData.get('pages');
+    const read = formData.get('read') === 'on';
+
+    addBookToLibrary(title, author, pages, read);
+
+    displayBooks();
+    form.reset();
+    dialog.close();
+})
