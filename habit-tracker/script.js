0d97cbf4259afd949df42ee46a9c355deb8a76bb
megamemma <sireneviyy@gmail.com>
2026-07-22 13:56:12 +0500

    Add simple function for days count, looping through and color changing depending on date

--- habit-tracker/script.js
+++ habit-tracker/script.js
@@ -42,3 +42,28 @@ const daysInThisMonth = daysInTheMonthList[currentMonth];
 
 let daysCompleted = 0;
 const totalDays = document.getElementById("totalDays");
+
+// SET UP THE CALENDAR DAYS: 
+let dayCount = 0;
+let rowCount = 0;
+const days = document.getElementsByClassName("days");
+
+for (let i = 0; i < days.length; i++) {
+    let day = days[rowCount].getElementsByClassName("day");
+    for (var j=0; j < day.length; j++) {
+        if(dayCount == currentDate - 1) {
+            day[j].setAttribute("style", "background-color: rgb(251, 187, 187)")
+            day[j].setAttribute("style", "border: 2px solid black");
+        }
+
+        if(dayCount < daysInThisMonth) {
+            day[j].innerHTML = dayCount + 1;
+            day[j].setAttribute("id", "day" + (dayCount + 1));
+            dayCount++;
+        } else {
+            day[j].innerHTML = "";
+            day[j].setAttribute("style", "background-color: white");
+        }
+    } 
+    rowCount++;
+}
