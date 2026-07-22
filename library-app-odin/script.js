c141fc6581b5a6e5ccd9cb8fc1319f654fc570a0
megamemma <sireneviyy@gmail.com>
2026-07-22 09:31:44 +0500

    Add a new project folder

--- library-app-odin/script.js
+++ library-app-odin/script.js
@@ -9,6 +9,10 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
+
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 
@@ -21,23 +25,40 @@ function displayBooks() {
     const grid = document.querySelector(".grid");
     grid.innerHTML = "";
 
-    myLibrary.forEach(  (book) => {
+    myLibrary.forEach((book) => {
         const card = document.createElement("div");
         card.classList.add("card");
+        card.setAttribute("data-id", book.id);
+
         card.innerHTML = `
             <h3>${book.title}</h3>
             <p>Author: ${book.author}</p>
             <p>Pages: ${book.pages}</p>
             <p>Status: ${book.read ? "Read already" : "Not read"}</p>
         `;
+
+        const delBtn = document.createElement("button");
+        delBtn.textContent = "Remove";
+        delBtn.onclick = () => {
+            const index = myLibrary.indexOf(book);
+            myLibrary.splice(index, 1);
+            displayBooks();
+        };
+
+        const toggleBtn = document.createElement("button");
+        toggleBtn.textContent = "Toggle Read";
+        toggleBtn.onclick = () => {
+            book.toggleRead(); 
+            displayBooks();
+        };
+
+        card.appendChild(toggleBtn);
+        card.appendChild(delBtn);
         grid.appendChild(card);
     });
 }
 
 addBookToLibrary("The Hobbit", "J. Tolkien", 295, false);
-addBookToLibrary("1984", "G. Orwell", 328, true);
-addBookToLibrary("1984", "G. Orwell", 328, true);
-addBookToLibrary("1984", "G. Orwell", 328, true);
 addBookToLibrary("1984", "G. Orwell", 328, true);
 addBookToLibrary("1984", "G. Orwell", 328, true);
 
@@ -56,11 +77,10 @@ form.addEventListener("submit", (e)=> {
     const title = formData.get('title');
     const author = formData.get('author');
     const pages = formData.get('pages');
-    const read = formData.get('read') === 'on';
 
+    const read = form.elements['read'].checked;
     addBookToLibrary(title, author, pages, read);
-
     displayBooks();
     form.reset();
     dialog.close();
-})
+});
