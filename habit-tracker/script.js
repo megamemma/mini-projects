9260d97d32ceb03eee9dbaa0b6ca930ae1dd7236
megamemma <sireneviyy@gmail.com>
2026-07-22 11:31:54 +0500

    Fetch date in JS, set months

---
+++ habit-tracker/script.js
@@ -0,0 +1,22 @@
+// GET THE DATE 
+
+let date = new Date();
+let currentMonth = date.getMonth();
+let currentDay = date.getDay();
+let currentDate = date.getDate();
+let currentYear = date.getFullYear();
+
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
